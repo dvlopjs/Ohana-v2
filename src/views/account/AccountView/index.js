35d3f3a1fee4fea707ea8572bf6296fc396659ec
dvlopjs <crissenn99@gmail.com>
2024-08-14 17:22:24 -0300

    Add circular progress user settings

--- src/views/account/AccountView/index.js
+++ src/views/account/AccountView/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {
   Box,
+  CircularProgress,
   Container,
   Divider,
   makeStyles,
@@ -26,17 +27,21 @@ const AccountView = () => {
   const classes = useStyles();
   const [currentTab, setCurrentTab] = useState('general');
   const [userData, setUserData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetchUser();
   }, []);
 
   const fetchUser = async () => {
+    setIsLoading(true);
     try {
       const response = await API.getInfoUser();
       setUserData(response);
+      setIsLoading(false);
     } catch (err) {
       console.error(err);
+      setIsLoading(false);
     }
   };
 
@@ -52,29 +57,48 @@ const AccountView = () => {
   };
 
   return (
-    <Page className={classes.root} title='Settings'>
-      <Container maxWidth='lg'>
+    <Page className={classes.root} title="Settings">
+      <Container maxWidth="lg">
         <Header />
-        <Box mt={3}>
-          <Tabs
-            onChange={handleTabsChange}
-            scrollButtons='auto'
-            value={currentTab}
-            variant='scrollable'
-            textColor='secondary'
+        {isLoading ? (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            position="fixed"
+            top={0}
+            left={0}
+            width="100%"
+            height="100vh"
           >
-            {tabs.map(tab => (
-              <Tab key={tab.value} label={tab.label} value={tab.value} />
-            ))}
-          </Tabs>
-        </Box>
-        <Divider />
-        <Box mt={3}>
-          {currentTab === 'general' && userData && <General user={userData} />}
-          {/*{currentTab === 'subscription' && <Subscription />}*/}
-          {/*{currentTab === 'notifications' && <Notifications />}*/}
-          {currentTab === 'security' && <Security />}
-        </Box>
+            <CircularProgress />
+          </Box>
+        ) : (
+          <>
+            <Box mt={3}>
+              <Tabs
+                onChange={handleTabsChange}
+                scrollButtons="auto"
+                value={currentTab}
+                variant="scrollable"
+                textColor="secondary"
+              >
+                {tabs.map(tab => (
+                  <Tab key={tab.value} label={tab.label} value={tab.value} />
+                ))}
+              </Tabs>
+            </Box>
+            <Divider />
+            <Box mt={3}>
+              {currentTab === 'general' && userData && (
+                <General user={userData} />
+              )}
+              {/*{currentTab === 'subscription' && <Subscription />}*/}
+              {/*{currentTab === 'notifications' && <Notifications />}*/}
+              {currentTab === 'security' && <Security />}
+            </Box>
+          </>
+        )}
       </Container>
     </Page>
   );
